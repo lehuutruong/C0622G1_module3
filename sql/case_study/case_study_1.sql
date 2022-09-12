4081e86811f13d2fee49024309572de14a2fba79
TRƯỜNG KHANG <lehuutruongkhang@gmail.com>
2022-09-12 16:03:28 +0700

    12/09

--- sql/case_study/case_study_1.sql
+++ sql/case_study/case_study_1.sql
@@ -23,16 +23,20 @@ so_dien_thoai varchar(45),
 email varchar(45),
 dia_chi varchar(45),
 ma_vi_tri int,
+foreign key (ma_vi_tri) references vi_tri(ma_vi_tri),
 ma_trinh_do int,
-ma_bo_phan int
+foreign key (ma_trinh_do) references trinh_do(ma_trinh_do),
+ma_bo_phan int,
+foreign key (ma_bo_phan) references bo_phan(ma_bo_phan)
 );
 create table loai_khach(
-ma_khach int primary key,
+ma_loai_khach int primary key,
 ten_loai_khach varchar(45)
 );
 create table khach_hang(
 ma_khach_hang int primary key,
-ma_loai_khach_hang int,
+ma_loai_khach int,
+foreign key (ma_loai_khach) references loai_khach(ma_loai_khach),
 ho_ten varchar(45),
 ngay_sinh date,
 gioi_tinh bit(1),
@@ -55,7 +59,9 @@ dien_tich int,
 chi_phi_thue double,
 so_nguoi_toi_da int,
 ma_kieu_thue int,
+foreign key (ma_kieu_thue) references kieu_thue(ma_kieu_thue),
 ma_loai_dich_vu int,
+foreign key(ma_loai_dich_vu) references loai_dich_vu(ma_loai_dich_vu),
 tieu_chuan_phong varchar(45),
 mo_ta_tien_nghi_khac varchar(45),
 dien_tich_ho_boi double,
@@ -68,8 +74,11 @@ ngay_lam_hop_dong datetime,
 ngay_ket_thuc datetime,
 tien_dat_coc double,
 ma_nhan_vien int,
+foreign key (ma_nhan_vien) references nhan_vien(ma_nhan_vien),
 ma_khach_hang int,
-ma_dich_vu int
+foreign key (ma_khach_hang) references khach_hang(ma_khach_hang),
+ma_dich_vu int,
+foreign key(ma_dich_vu) references dich_vu(ma_dich_vu)
 );
 create table dich_vu_di_kem(
 ma_dich_vu_di_kem int primary key,
@@ -81,5 +90,7 @@ trang_thai varchar(45)
 create table hop_dong_chi_tiet(
 ma_hop_dong_chi_tiet int primary key,
 ma_hop_dong int,
+foreign key (ma_hop_dong) references hop_dong(ma_hop_dong),
 ma_dich_vu_di_kem int,
+foreign key (ma_dich_vu_di_kem) references dich_vu_di_kem(ma_dich_vu_di_kem),
 so_luong int);
